028fe40c5866b5458dba25f54bac3ac0e211b3d5
Adithyahari vpn <68626434+Adithyaharivpn@users.noreply.github.com>
2024-09-24 15:23:54 +0530

    Add files via upload

--- JS1/form.js
+++ JS1/form.js
@@ -2,20 +2,30 @@ var user=document.getElementById("user")
 var pass=document.getElementById("pass")
 var button=document.getElementById("btn")
 var error=document.getElementById("error")
+// function validate() {
+//     if (user.value.trim()=="" || pass.value.trim()=="") {
+//         alert("Fleid cannot be empty");
+//         return false
+//     }
+//     else if(pass.value.length<8){
+//         //alert("Password should be 8 characters long")
+//         pass.style.border="2px solid red";
+//         error.textContent="Password should be 8 characters long";
+//         return false
+//     }
+//     else {
+//         pass.style.border="2px solid green";
+//         error.textContent="";
+//         return true
+//     }
+// }
 function validate() {
-    if (user.value.trim()=="" || pass.value.trim()=="") {
-        alert("Fleid cannot be empty");
-        return false
+    let regex= /^([a-zA-Z0-9.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/
+    if (regex.test(user.value)) {
+        return true;
+    } else {
+        alert("Email is invalid")
+        return false;
     }
-    else if(pass.value.length<8){
-        //alert("Password should be 8 characters long")
-        pass.style.border="2px solid red";
-        error.textContent="Password should be 8 characters long";
-        return false
-    }
-    else {
-        pass.style.border="2px solid green";
-        error.textContent="";
-        return true
-    }
+    
 }
